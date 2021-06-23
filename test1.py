2b743b287e5c00d52f02b80dab701a4a2d648991
Alexandre <alexandreoliveira2001g@gmail.com>
2021-06-23 16:17:55 +0100

    added things

--- test1.py
+++ test1.py
@@ -1,9 +1,6 @@
-
-
 class Student:
     def __init__(self, name , major, gpa, is_on_probation):
         self.name = name
         self.major = major
         self.gpa = gpa
         self.in_on_probation = is_on_probation
-    
