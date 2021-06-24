9aa6f4d9e1e7489c1b9c590303e6bb274a104910
Alexandre <alexandreoliveira2001g@gmail.com>
2021-06-24 15:50:27 +0100

    changes

--- student.py
+++
@@ -1,6 +0,0 @@
-from test1 import Student
-
-students = [Student("Jim", "Business", 3.1, False),Student("Alex", "Aeronautical", 7, True)]
-
-for student in students:
-    print(student.name)
--- test1.py
+++
@@ -1,6 +0,0 @@
-class Student:
-    def __init__(self, name , major, gpa, is_on_probation):
-        self.name = name
-        self.major = major
-        self.gpa = gpa
-        self.in_on_probation = is_on_probation
